cb2e4fa3bbc4e78641fb7fdac2ec23302acbe31b
Margaret Donin <rtdonin@gmail.com>
2021-01-21 19:56:28 -0500

    AngularJS Assignment

---
+++ AngularJs-Assignments/Module1/app.js
@@ -0,0 +1,33 @@
+(function () {
+    'use-strict';
+
+    angular.module('LunchCheckApp', [])
+        .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope) {
+
+        $scope.checkLunch = function () {
+            const lunchItems = $scope.lunch.split(",");
+            let numberItems = 0;
+
+            for (let i = 0; i < lunchItems.length; i++) {
+                // check the lunchItem isn't whitespace and has characters.
+                if (lunchItems[i].trim() != "" && lunchItems[i] != null) {
+                    numberItems++;
+                }
+            }
+
+            if (numberItems > 3) {
+                $scope.message = "Too much!";
+            } else if (numberItems > 0) {
+                $scope.message = "Enjoy!";
+            } else {
+                $scope.message = "Please enter data first";
+            }
+        };
+
+    }
+
+})();
