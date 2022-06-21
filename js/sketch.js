ae2994e2f7fc4b96c0c5670c902491edaae5d41c
Margaret Donin <rtdonin@gmail.com>
2022-06-21 16:37:25 -0400

    Edited main page. Everything is nearly in the right place. Some minor details need to be fixed still

---
+++ js/sketch.js
@@ -0,0 +1,8 @@
+var cCircles = new cCircles();
+var lCircles = new lCircles();
+var cTriangles = new cTriangles();
+var lTriangles = new lTriangles();
+var cSquares = new cSquares();
+var lSquares = new lSquares();
+
+
