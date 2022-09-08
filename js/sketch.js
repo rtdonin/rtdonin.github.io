90ec6376ec478c8de201fd6a024c544049b24acc
Margaret Donin <rtdonin@gmail.com>
2022-09-08 18:55:25 -0400

    email and about me updates

--- js/sketch.js
+++
@@ -1,8 +0,0 @@
-var cCircles = new cCircles();
-var lCircles = new lCircles();
-var cTriangles = new cTriangles();
-var lTriangles = new lTriangles();
-var cSquares = new cSquares();
-var lSquares = new lSquares();
-
-
